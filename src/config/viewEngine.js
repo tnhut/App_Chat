b85c4a336ccf6d590d9313caa7d3d7c02c5bfae8
tnhut <thanhnhut2506@gmail.com>
2021-09-11 20:32:45 +0700

    [1.0.0] Add static pages

---
+++ src/config/viewEngine.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import expressEjsExtend from 'express-ejs-extend';
+
+/*
+Config View Engine
+*/ 
+
+let configViewEngine=(app)=>{
+    app.use(express.static("./src/public"));
+    app.engine("ejs", expressEjsExtend);
+    app.set("view engine","ejs");
+    app.set("views","./src/views");
+
+    
+};
+
+module.exports=configViewEngine;
