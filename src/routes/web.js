2066c5a855b1c70ffea3421dfc05bc47f35a3dd2
tnhut <thanhnhut2506@gmail.com>
2021-09-11 21:30:44 +0700

    [1.0.0] Add routes,controllers

---
+++ src/routes/web.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {home,auth} from "./../controllers/index";
+
+let router=express.Router();
+
+/* 
+Init all routes
+*/
+
+let initRoutes= (app)=>{
+    router.get("/", home.getHome);
+     
+    router.get("/login-register", auth.getLoginRegister);
+
+    router.get("/logout", auth.getLogout);
+
+    return app.use("/", router);
+};
+
+module.exports=initRoutes;
