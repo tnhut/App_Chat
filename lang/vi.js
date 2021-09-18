34a0e5bba8a55fdb079413a8b3e5b670261b1daa
tnhut <thanhnhut2506@gmail.com>
2021-09-18 21:38:02 +0700

    [1.0.0] Send Email confirm Account

--- lang/vi.js
+++ lang/vi.js
@@ -8,12 +8,27 @@ export const transValidation={
 export const transErrors={
     account_in_use:"Email này đã được sử dụng",
     account_remove:"Tài khoản này đã bị gỡ khỏi hệ thống. Vui lòng liên hệ với team support",
-    account_not_active:"Email này đã được đăng ký nhưng chưa active tài khoản. Vui lòng kiểm tra Email"
+    account_not_active:"Email này đã được đăng ký nhưng chưa active tài khoản. Vui lòng kiểm tra Email",
+    token_underfined:"Token không tồn tại!"
     
 };
 
 export const transSuccess={
     userCreated: (userEmail)=>{
         return `Tài khoản <strong>${userEmail}</strong> đã được tạo. Vui lòng kiểm tra Email để active tài khoản `;
-    }
-}
+    },
+    account_actived:"Kích hoạt tài khoản thành công. Bạn có thể đăng nhập được vào ứng dụng"
+};
+
+export const transMail={
+    subject:"Xác nhận kích hoạt tài khoản",
+    template:(linkVerify)=>{
+        return `
+        <h2>Bạn nhận được email này vì đã đăng ký tài khoản trên AppChat</h2>
+        <h3>Vui lòng click vào liên kết bên dưới để xác nhận tài khoản </h3>
+        <h3><a href="${linkVerify}" target="blank">${linkVerify}</a></h3>
+        <h4>Trân trọng.</h4>
+        `
+    },
+    send_failed:"Có lỗi trong quá trình gửi email, vui lòng liên hệ với bộ phận hỗ trợ"
+};
