d1853305cb9eb021c18e2c152e846f1bae723241
tnhut <thanhnhut2506@gmail.com>
2021-09-19 21:14:06 +0700

    [1.0.0] Login Facebook

--- sh/env.example.sh
+++ sh/env.example.sh
@@ -15,3 +15,8 @@ export MAIL_USER=***
 export MAIL_PASSWORD=***
 export MAIL_HOST=***
 export MAIL_PORT=***
+
+#Connfig facebook login app
+export FB_APP_ID=***
+export FB_APP_SECRET=***
+export FB_CALLBACK_URL=***
