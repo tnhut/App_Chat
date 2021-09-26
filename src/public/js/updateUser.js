f66ab04fa9fb66325108cd6103f5008605d8df0d
tnhut <thanhnhut2506@gmail.com>
2021-09-26 22:52:59 +0700

    [1.0.2] Update avatar user, handle backend

--- src/public/js/updateUser.js
+++ src/public/js/updateUser.js
@@ -7,17 +7,17 @@ function updateUserInfo(){
         let match=["image/png","image/jpg","image/jpeg"];
         let limit=1048576; // byte=1M
 
-        if($.inArray(fileData.type,match)===-1){
-            alertify.notify("Kiểu dữ liệu không hợp lệ","error",7);
-            $(this).val(null);
-            return false;
-        }
+        // if($.inArray(fileData.type,match)===-1){
+        //     alertify.notify("Kiểu File không hợp lệ","error",7);
+        //     $(this).val(null);
+        //     return false;
+        // }
 
-        if(fileData.size >limit){
-            alertify.notify("Ảnh upload cho phép tối đa 1M","error",7);
-            $(this).val(null);
-            return false;
-        }
+        // if(fileData.size >limit){
+        //     alertify.notify("Ảnh upload cho phép tối đa 1M","error",7);
+        //     $(this).val(null);
+        //     return false;
+        // }
         
         if(typeof(FileReader)!="undefined"){
             let imagePreview=$("#image-edit-profile");
@@ -79,10 +79,21 @@ function saveUserInfo(){
             processData:false,
             data:userAvatar,
             success:function(result){
-
+                // Show text notification
+                $(".user-modal-alert-success").find("span").text(result.message);
+                $(".user-modal-alert-success").css("display","block");
+                // Update avatar on navbar
+                $("#navbar-avatar").attr("src",result.imageSrc);
+                // Update originAvatarSrc to new
+                originAvatarSrc=result.imageSrc;
+                $("#input-btn-cancel-update-user").click();
             },
             error:function(error){
                 
+                $(".user-modal-alert-error").find("span").text(error.responseText);
+                $(".user-modal-alert-error").css("display","block");
+                // reset all
+                $("#input-btn-cancel-update-user").click();
             }
         })
         // console.log(userAvatar);
@@ -94,6 +105,7 @@ function cancelUserInfo(){
     $("#input-btn-cancel-update-user").bind("click", function(){
          userAvatar=null;
          userInfo={};
+         $("#input-change-avatar").val(null);
          $("#user-modal-avatar").attr('src',originAvatarSrc);
     })
 }
