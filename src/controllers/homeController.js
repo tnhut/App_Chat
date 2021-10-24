d685c1f27e4c442876a98fad5be319718a73b665
tnhut <thanhnhut2506@gmail.com>
2021-10-24 23:23:42 +0700

    [1.0.0] Count unread Notification and design modal

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,13 +1,16 @@
 import {notification} from "./../services/index";
 
 let getHome=async(req,res)=>{
+   // Get 10 item once
    let notifications=await notification.getNotifications(req.user._id);
-
+   // get amout notification unread
+   let countNotifUnread=await notification.countNotifUnread(req.user._id);
     return res.render("main/home/home",{
         errors: req.flash("errors"),
         success: req.flash("success"),
         user:req.user,
-        notifications:notifications
+        notifications:notifications,
+        countNotifUnread:countNotifUnread
     });
 };
 
--- src/models/notificationModel.js
+++ src/models/notificationModel.js
@@ -35,6 +35,19 @@ NotificationSchema.statics={
         return this.find({
             "receiverId":userId
         }).sort({"createdAt":-1}).limit(limit).exec();
+    },
+
+    /**
+     * Count all notifaction unread
+     * @param {String} userId 
+     */
+    countNotifUnread(userId){
+        return this.count({
+            $and:[
+                {"receiverId":userId},
+                {"isRead":false},
+            ]
+        }).exec();
     }
 }
 
@@ -46,16 +59,16 @@ const NOTIFICATION_CONTENTS={
     getContent:(notificationType,isRead,userId,userName,userAvatar)=>{
         if(notificationType===NOTIFICATION_TYPES.ADD_CONTACT){
             if(!isRead){
-                return `<span class="notif-readed-false" data-uid="${ userId }">
+                return `<div class="notif-readed-false" data-uid="${ userId }">
                             <img class="avatar-small" src="images/users/${userAvatar}"alt="">
                             <strong>${userName}</strong> đã gửi cho bạn lời mời kết bạn!
-                        </span><br><br><br>`;
+                        </div>`;
             }
 
-            return `<span  data-uid="${ userId }">
+            return `<div  data-uid="${ userId }">
                         <img class="avatar-small" src="images/users/${userAvatar}"alt="">
                         <strong>${userName}</strong> đã gửi cho bạn lời mời kết bạn!
-                    </span><br><br><br>`;
+                    </div>`;
            
         }
         return "No matching with any notification type";
